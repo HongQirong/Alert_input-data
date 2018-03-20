35a2d4f6406b7e436f32e00e6b38f104d6ced01c
x <xcode@xs-Mac.local>
2018-03-20 10:24:54 +0800

    Initial Commit

---
+++ Alert_input dataTests/Alert_input_dataTests.swift
@@ -0,0 +1,36 @@
+//
+//  Alert_input_dataTests.swift
+//  Alert_input dataTests
+//
+//  Created by x on 2018/3/20.
+//  Copyright © 2018年 x. All rights reserved.
+//
+
+import XCTest
+@testable import Alert_input_data
+
+class Alert_input_dataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
